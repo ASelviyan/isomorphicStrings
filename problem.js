7cb493124704c1c7cefbebd099542ee703503c24
ASelviyan <angelikaselviyan5@gmail.com>
2022-08-31 18:19:33 -0600

    Solved

--- problem.js
+++ problem.js
@@ -5,8 +5,7 @@
 // if the letters where switched the words would stay the same (in spelling)
 // no two characters will map to the same character but one character can map to it self
 
-//PSEUDOCODE 
-// turn both the string that are given into an array of characters 
+//PSEUDOCODE  
 // create an object that will keep track of the pairing characters 
 // make a for loop that will iterate through both of the string arrays 
 // each time it iterate you will have conditional to do through 
@@ -19,3 +18,26 @@
 // the length of the t string will always be the same length of the s string  
 // s and t strings consist of any valid ascii characters 
 
+// SOLUTION
+var isIsomorphic = function (s, t) {
+    // create an object that will keep track of the pairing characters 
+    const pairing = {}
+    // make a for loop that will iterate through both of the string arrays 
+    for (let i = 0; i < s.length; i++) {
+        // each time it iterate you will have conditional to do through 
+        // 1. check if the index we are at now is in the object if not the add the two index values to the obejct as key value pairs
+        if (!(s[i] in pairing)) {
+            if (Object.values(pairing).includes(t[i])) {
+                return false
+            }
+            pairing[s[i]] = t[i]
+            // 2. if the indices are already in the object check if the two indeices have the corrent pairiing if they dont return false
+        } else if (!(pairing[s[i]] === t[i])) {
+            return false
+        }
+
+    }
+
+    // 3. if they do pair up keep iterating through the loop and at the end retrun true
+    return true
+};
